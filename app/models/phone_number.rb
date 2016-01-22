384b5390ac6d0e93f7d626f4d9ee2a8a514beb34
Katrina Eaton <katrina.eaton@gmail.com>
2016-01-21 22:16:49 -0800

    regex validation for phone number creation

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,4 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
   # Remember to create a migration!
   validates :number, presence: true, uniqueness: true
+  validates_format_of :number, with: /\A\d{3}-\d{3}-\d{4}\z/, on: :create
 end
